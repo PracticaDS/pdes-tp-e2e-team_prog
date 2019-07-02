d87a3b2dc04a7abff973b1a352d7ddf43e9282e7
Jesus Laime <fedecapx>
2019-07-01 23:53:33 -0300

    fix commit message

---
+++ cypress/integration/create-game.spec.js
@@ -0,0 +1,21 @@
+describe('Create Game', () => {
+  it('creates a new game', () => {
+    cy.fixture('user.json').then(fixture_user => {
+      const user = fixture_user.new
+
+      cy.login(user)
+      cy.get('[component_name="create-game-open-button"]').click()
+      cy.get('[component_name="create-game-input"]')
+        .find('input')
+        .type(user.game_name)
+      cy.get('[component_name="create-game-rows"]')
+        .find('input')
+        .type(user.game_rows)
+      cy.get('[component_name="create-game-columns"]')
+        .find('input')
+        .type(user.game_columns)
+      cy.get('[component_name="create-game-create-button"]').click()
+      cy.url().should('include', '/game')
+    })
+  })
+})
